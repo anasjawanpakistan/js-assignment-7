134cb444c320dd97cc8fda95b0ebb89bf0fd4dd8
anasjawanpakistan <anasm0213@gmail.com>
2023-10-09 15:08:38 +0500

    Assignment

---
+++ app.js
@@ -0,0 +1,33 @@
+// function alertMessage(){
+//     var alertMessage = +prompt("Enter your message")
+//     var alertLength = +prompt("Enter your length")
+
+//     var i = 1;
+//     while(i <=alertLength){
+//         function message(){
+//             alert("Hello")
+//         }
+//     }
+// } 
+
+
+
+
+// function playsSound(){
+//     var playsSound = +prompt("Enter your sound")
+//     var playsLength = +prompt("Enter your length")
+
+// var i = 1;
+// while(i <=playsLength){
+//     function sound(){
+//         alert("World")
+//     }
+// }
+
+
+// }
+
+
+
+
+
